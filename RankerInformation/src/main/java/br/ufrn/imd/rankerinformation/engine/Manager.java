671e71b7e2de2468b75b60a7f956dc692f45d812
Jackson Rauup <jackson.rauup.info@gmail.com>
2016-11-15 17:23:51 -0300

    GenreMatchingModelAssociation created

--- RankerInformation/src/main/java/br/ufrn/imd/rankerinformation/engine/Manager.java
+++ RankerInformation/src/main/java/br/ufrn/imd/rankerinformation/engine/Manager.java
@@ -10,6 +10,7 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 import br.ufrn.imd.rankerinformation.db.DBGenerator;
+import br.ufrn.imd.rankerinformation.engine.filter.GenreMatchingModelAssociation;
 import br.ufrn.imd.rankerinformation.engine.filter.IntersectionModelAssociation;
 import br.ufrn.imd.rankerinformation.engine.filter.IntersectionModelAssociation2;
 import br.ufrn.imd.rankerinformation.engine.search.MoviesSearchEngine;
@@ -44,7 +45,7 @@ public class Manager {
 		
 		
 		//Cria um segundo usuário (mesmo processo anterior)
-		User user2 = new User(2, "Jão");
+		User user2 = new User(2, "Raul");
 		List<SourceData> listSourceData2= new ArrayList<>();
 		listSourceData2.add(new SourceData(10, "Bad", 4));
 		listSourceData2.add(new SourceData(20, "uhhh", 1));
@@ -54,6 +55,16 @@ public class Manager {
 		mcl2.setup(user2, listSourceData2);
 		mcl2.setModelAssociation(new IntersectionModelAssociation2());
 		
+		//Cria um segundo usuário (mesmo processo anterior)
+		User user3 = new User(3, "Jackson");
+		List<SourceData> listSourceData3= new ArrayList<>();
+		listSourceData3.add(new SourceData(11, "Aventura", 5));
+		listSourceData3.add(new SourceData(22, "Infantil", 3));
+		listSourceData3.add(new SourceData(33, "Ação", 2));
+		listSourceData3.add(new SourceData(44, "Drama", 4));
+		ManagerCycleLife mcl3 = new ManagerCycleLife(user3.getId());
+		mcl3.setup(user3, listSourceData3);
+		mcl3.setModelAssociation(new GenreMatchingModelAssociation());
 		
 		
 		/*Implementa uma ferrmenta de busca*/
@@ -118,6 +129,7 @@ public class Manager {
 		//Adiciona um observador ao gerente de busca ()
 		sn.addObserver(mcl);
 		sn.addObserver(mcl2);
+		sn.addObserver(mcl3);
 		
 		
 		//Start no processo de busca
