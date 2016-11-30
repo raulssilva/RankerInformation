34d478ee59c1dcffaaa2f9f7c61e288fef6d1dae
Jackson Rauup <jackson.rauup.info@gmail.com>
2016-11-29 23:49:34 -0300

    CategoryMatchingModelAssociation updated

--- ranker-books-information/src/main/java/ranker_books_information/CategoryMatchingModelAssociation.java
+++ ranker-books-information/src/main/java/ranker_books_information/CategoryMatchingModelAssociation.java
@@ -1,16 +1,15 @@
-package br.ufrn.imd.rankerinformation.engine.filter.modelsassociation;
+package ranker_books_information;
 
 import br.ufrn.imd.rankerinformation.engine.filter.ModelAssociation;
 import br.ufrn.imd.rankerinformation.user.model.SourceData;
 
 public class CategoryMatchingModelAssociation implements ModelAssociation{
 
-	@Override
 	public double calculate(SourceData termPreferences, String termsInformation) {
 		String content = termPreferences.getContent().toLowerCase();		
 		double weight = termPreferences.getWeight();
-	
-		if(content.equals(termsInformation)) {
+		
+		if(termsInformation.toLowerCase().contains(content)) {
 			return weight;
 		} else {
 			return 0.0;
