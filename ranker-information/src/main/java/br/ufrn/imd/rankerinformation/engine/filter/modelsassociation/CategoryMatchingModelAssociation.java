711c7463f55b35e59db658fb80f62291f5d44a14
Jackson Rauup <jackson.rauup.info@gmail.com>
2016-11-29 23:25:14 -0300

    CategoryMatchingModelAssociation created

---
+++ ranker-information/src/main/java/br/ufrn/imd/rankerinformation/engine/filter/modelsassociation/CategoryMatchingModelAssociation.java
@@ -0,0 +1,20 @@
+package br.ufrn.imd.rankerinformation.engine.filter.modelsassociation;
+
+import br.ufrn.imd.rankerinformation.engine.filter.ModelAssociation;
+import br.ufrn.imd.rankerinformation.user.model.SourceData;
+
+public class CategoryMatchingModelAssociation implements ModelAssociation{
+
+	@Override
+	public double calculate(SourceData termPreferences, String termsInformation) {
+		String content = termPreferences.getContent().toLowerCase();		
+		double weight = termPreferences.getWeight();
+	
+		if(content.equals(termsInformation)) {
+			return weight;
+		} else {
+			return 0.0;
+		}
+	}
+	
+}
